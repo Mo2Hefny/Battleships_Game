6d52be8deb2922b65b16c8df2246a2d26875e032
Mo2Hefny <moamensaied1@gmail.com>
2023-01-23 12:04:26 +0200

    Preparations

--- Battleships.cpp
+++ Battleships.cpp
@@ -1,11 +1,27 @@
 // Battleships.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
