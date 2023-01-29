2f024e47871a97e228791f42e47f0d4141c6e39f
Mo2Hefny <moamensaied1@gmail.com>
2023-01-29 03:41:22 +0200

    Added tests

--- UI/Grid.cpp
+++ UI/Grid.cpp
@@ -9,21 +9,51 @@ Grid::Grid()
 			grid[i][j].setSize(Vector2f(25, 25));
 
 			//Set color half transparent
-			grid[i][j].setFillColor(sf::Color(255, 255, 255, 128));
+			grid[i][j].setFillColor(UI.GridColor);
 		}
 	}
 }
 
-void Grid::draw(RenderWindow& window, Player p)
+void Grid::draw(int player)
 {
-	int x = (p == player ) ? 100 : 600, y = 250;
+	int x = (player == 0 ) ? UI.player1->x : UI.player2->x, y = UI.player1->y;
 	for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 10; j++)
 		{
-			grid[i][j].setPosition(x + i * 30, y + j * 30);
-			window.draw(grid[i][j]);
+			grid[i][j].setPosition(x + j * 30, y + i * 30);
+			game_window->draw(grid[i][j]);
 		}
 	}
+}
+
+bool Grid::ColorShip(RenderWindow& window, Color& c, vector<Vector2i>& hitbox)
+{
+	bool empty_space = true;
+	for (int i = 0; i < hitbox.size(); i++)
+	{
+		if (grid[hitbox[i].x][hitbox[i].y].getFillColor() == c)
+			empty_space = false;
+		grid[hitbox[i].x][hitbox[i].y].setFillColor(Color::White);
+		grid[hitbox[i].x][hitbox[i].y].setFillColor(c);
+		//window.draw(grid[hitbox[i].x][hitbox[i].y]);
+	}
+	return false;
+}
+
+bool Grid::CheckValidity(Color c)
+{
+	int count = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (grid[i][j].getFillColor() == c)
+				count++;
+		}
+	}
+	if (count == 17)
+		return true;
+	return false;
 }
 
--- UI/Grid.h
+++ UI/Grid.h
@@ -4,15 +4,22 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "../DEFS.h"
+
 using namespace std;
 using namespace sf;
 
 class Grid
 {
 	RectangleShape grid[10][10];
+	RenderWindow* game_window;
 public:
 	Grid();
-	void draw(RenderWindow &game, Player p);
+	bool CheckValidity(Color c);
+	void draw(int player);
+	bool ColorShip(RenderWindow& window, Color& c, vector<Vector2i>&);
+
+	//setters
+	void setWindow(RenderWindow& window) { game_window = &window; }
 };
 
 #endif
